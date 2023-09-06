b4a256efdc8776453c36f65caa4973071e2a3e0b
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2023-09-06 10:57:52 +0200

    Authentication component init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPageComponent } from './auth-page/auth-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'auth', component: AuthPageComponent, 
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
