0e4420bfb70eef75978d1bb82d6df365ea16a6c3
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2023-09-09 16:54:09 +0200

    Loginform + authenticationservice implemented

--- src/app/auth-page/login-form/login-form.component.ts
+++ src/app/auth-page/login-form/login-form.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -14,6 +20,9 @@ export class LoginFormComponent {
     email: new FormControl(''),
     password: new FormControl(''),
   });
+
+  constructor(private router: Router) {}
+  users: string[] = [];
   getErrorMessage(email: AbstractControl) {
     console.log(email);
     /*if (value.email) {
@@ -27,6 +36,32 @@ export class LoginFormComponent {
 
   switchMode() {
     this.isLoginMode = !this.isLoginMode;
-    console.log(this.isLoginMode);
+  }
+
+  onLogin(): void {
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+    const user = users.find((user: any) => user.email === email);
+    console.log(user);
+
+    if (!user) {
+      alert('Email not found, please register!');
+    } else {
+      if (user.password === password) {
+        localStorage.setItem('loggedIn', 'true');
+        alert('You have successfully logged in!')
+        this.router.navigate(['']);
+      }
+    }
+    
+  }
+
+  onSignUp(): void {
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    users.push(this.loginForm.value);
+
+    localStorage.setItem('users', JSON.stringify(users));
+    console.log(this.loginForm.value);
   }
 }
