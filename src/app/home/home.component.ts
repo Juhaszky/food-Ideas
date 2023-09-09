7902829a2d2f94bc09ea33f10fdae2f1a12c9187
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2023-09-09 16:55:45 +0200

    welcome text implemented

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {CdkDrag} from '@angular/cdk/drag-drop';
+import { FormControl, FormGroup } from '@angular/forms';
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  providers: [CdkDrag]
+})
+export class HomeComponent {
+  selected!: Date | null;
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+  
+  private _mobileQueryListener: () => void;
+  mobileQuery!: MediaQueryList;
+  campaignOne = new FormGroup({
+    start: new FormControl(new Date(year, month, 13)),
+    end: new FormControl(new Date(year, month, 16)),
+  });
+}
